a99eb19a433ba8d6b62063f52d1957697d81a6a9
Sebastian <sebastian.raczylo@gmail.com>
2022-05-15 11:34:10 +0200

    podpunkty 7-12

--- src/DES.js
+++ src/DES.js
@@ -37,6 +37,94 @@ const TablePC =
     21, 13, 5, 28, 20, 12, 4
 ];
 
+const TablePC2 =
+[
+    14, 17, 11, 24, 1, 5,
+    3, 28, 15, 6, 21, 10,
+    23, 19, 12, 4, 26, 8,
+    16, 7, 27, 20, 13, 2,
+    41, 52, 31, 37, 47, 55,
+    30, 40, 51, 45, 33, 48,
+    44, 49, 39, 56, 34, 53,
+    46, 42, 50, 36, 29, 32 
+];
+
+const TableE =
+[
+    32, 1, 2, 3, 4, 5,
+    4, 5, 6, 7, 8, 9,
+    8, 9, 10, 11, 12, 13,
+    12, 13, 14, 15, 16, 17,
+    16, 17, 18, 19, 20, 21,
+    20, 21, 22, 23, 24, 25,
+    24, 25, 26, 27, 28, 29,
+    28, 29, 30, 31, 32, 1 
+];
+
+const S1 = 
+[
+    [14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7],
+    [0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8],
+    [4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0],
+    [15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13]
+]
+const S2 = 
+[
+    [15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10],
+    [3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5],
+    [0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15],
+    [13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9]
+]
+
+const S3 = 
+[
+    [10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8],
+    [13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1],
+    [13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7],
+    [1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12] 
+]
+
+const S4 = 
+[
+    [7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15],
+    [13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9],
+    [10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4],
+    [3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14]
+]
+
+const S5 = 
+[
+    [2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9],
+    [14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6],
+    [4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14],
+    [11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3] 
+]
+
+const S6 = 
+[
+    [12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11],
+    [10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8],
+    [9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6],
+    [4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13]
+]
+
+const S7 = 
+[
+    [4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1],
+    [13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6],
+    [1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2],
+    [6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12]
+]
+
+const S8 = 
+[
+    [13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7],
+    [1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2],
+    [7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8],
+    [2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11],
+]
+
+
 const TableShiftRound = [ 1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1 ];
 
 //************************************************************************************************************************************************************************************
@@ -87,17 +175,18 @@ function BytesToString(bytes)
     }
     return outputText;
 }
+
+
 //************************************************************************************************************************************************************************************
 //************************************************************************************************************************************************************************************
 //************************************************************************************************************************************************************************************
 
 
-//PRZEKSZTALCANIE TABEL
 function TablePermutation(inputText, tab, n){ 
-    let outputText = inputText;
+    let outputText = ""
     for (let a = 0; a < n; a++) {
-        //outputText.charAt(tab.charAt(a) - 1) = inputText.charAt(a);
-        outputText = setCharAt(outputText, tab[a] - 1, inputText.charAt(a))
+        
+        outputText += inputText[tab[a] - 1]
     }
 
     return outputText;
@@ -121,12 +210,82 @@ function shiftLeft(key, x){
     return newKey;
 }
 
+//XOR dla argumentow o tej samej dlugosci
+function XOR(arg1, arg2){
+    let length = arg1.length
+
+    let result = ""
+    for(let i = 0; i < length; i++){
+        if(arg1[i] != arg2[i]){
+            result += "1"
+        }
+        else{
+            result += "0"
+        }
+    }
+    return result
+}
+
+function BytesToBCD(code, length){
+    let resultBCD = 0
+    for(let i = length; i > 0; i--){
+        if(code[i - 1] == "1"){
+            resultBCD += Math.pow(2, length - i)
+        }
+    }
+    return resultBCD
+}
+
+
+function BCDtoBytes(number){
+    let resultBytes = ""
+
+
+    for(let i = 3; i >= 0; i--){
+
+        if(number >= Math.pow(2, i)){
+            resultBytes += "1"
+            number -= Math.pow(2, i)
+        }
+        else{
+            resultBytes += "0"
+        }
+    }
+
+
+    return resultBytes
+}
+
+
+
+function Sequence6bytesTo4bytes(position, table){
+    let column = position[0] + position[5]
+    let index = position.substring(1, 5)
+
+    let columnBCD = BytesToBCD(column, column.length)
+    let indexBCD = BytesToBCD(index, index.length)
+
+
+
+    let result = table[columnBCD][indexBCD]
+    // console.log("result " + result)
+    let result4bytes = BCDtoBytes(result)
+    // console.log("result4 " + result4bytes)
+
+    
+    return result4bytes
+}
+
 //************************************************************************************************************************************************************************************
 //----------GLOWNY ALGORYTM------------------
 //************************************************************************************************************************************************************************************
 function AlgorithmDES(inputText, key){
+    // console.log(inputText)
+    // console.log(key)
     let outputText = StringToBytes(inputText);
+    
     outputText = TablePermutation(outputText, TableIP, 64);
+
 
     //PODZIAL TEKSTU NA BLOKI: L i R
     let BlockL = outputText.substring(0, 32);
@@ -136,23 +295,55 @@ function AlgorithmDES(inputText, key){
     let keyByte = StringToBytes(key);
     keyByte = TablePermutation(keyByte, TablePC, 56);
     let keyByte2 = "";
-    for (let a = 0; a < 64; a++) {
-        if (((a + 1) % 8) !== 0) {
-            keyByte2 += keyByte.charAt(a);
-        }
-    }
+
+    // for (let a = 0; a < 64; a++) {
+    //     if (((a + 1) % 8) !== 0) {  
+    //         keyByte2 += keyByte.charAt(a);          // chyba nie potrzeba sprawia ze z 56 bitowego klucza mamy 49 bity
+    //     }
+    // }
 
     //PODZIAL KLUCZA NA BLOKI: C i D
-    let BlockC = keyByte2.substr(0, 28);
-    let BlockD = keyByte2.substr(28, 56);
+    let BlockC = keyByte.substring(0, 28);
+    let BlockD = keyByte.substring(28, 56);
 
+    
     for (let a = 0; a < 16; a++) {
         BlockC = shiftLeft(BlockC, TableShiftRound[a]);
         BlockD = shiftLeft(BlockD, TableShiftRound[a]);
         let newKey = BlockC + BlockD;
         //TUTAJ MAJA SIE ODBYWAC TYCH 16 RUND
+ 
+        // Sebastian code
+        let Key48 = TablePermutation(newKey, TablePC2, 48) // krok 7
+        
+        let BlockR48 = TablePermutation(BlockR, TableE, 48) // krok 8
+
+        //mozliwe ze odtad powinno nie byc w pentli
+        let xorResult = XOR(Key48, BlockR48) // krok 9
+        
+        let bytes6Sequence1 = xorResult.substring(0, 6);  //krok 10
+        let bytes6Sequence2 = xorResult.substring(6, 12);
+        let bytes6Sequence3 = xorResult.substring(12, 18);
+        let bytes6Sequence4 = xorResult.substring(18, 24);
+        let bytes6Sequence5 = xorResult.substring(24, 30);
+        let bytes6Sequence6 = xorResult.substring(30, 36);
+        let bytes6Sequence7 = xorResult.substring(36, 42);
+        let bytes6Sequence8 = xorResult.substring(42, 48);
+
+        let bytes4Sequence1 = Sequence6bytesTo4bytes(bytes6Sequence1, S1) // krok 11, 12
+        let bytes4Sequence2 = Sequence6bytesTo4bytes(bytes6Sequence2, S2)
+        let bytes4Sequence3 = Sequence6bytesTo4bytes(bytes6Sequence3, S3)
+        let bytes4Sequence4 = Sequence6bytesTo4bytes(bytes6Sequence4, S4)
+        let bytes4Sequence5 = Sequence6bytesTo4bytes(bytes6Sequence5, S5)
+        let bytes4Sequence6 = Sequence6bytesTo4bytes(bytes6Sequence6, S6)
+        let bytes4Sequence7 = Sequence6bytesTo4bytes(bytes6Sequence7, S7)
+        let bytes4Sequence8 = Sequence6bytesTo4bytes(bytes6Sequence8, S8)
+
+        // console.log("ciag 4 bitowy" + test + " " + test.length)
+        //Sebastian code end
     }
 
+    
 
     //TO TYLKO DO SPRAWDZENIA - wydaje mi sie ze dopiero pozniej przy innych podpunktach trzeba to wykorzystac
     outputText = TablePermutation(outputText, TableIP1, 64);
